d6345736d610a02629efe5021fb9c6745440103c
vijayvp09 <vvpillai95@gmail.com>
2024-05-09 23:42:07 +0530

    created basic api

---
+++ api/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mysql =  require("mysql");
+const app = express();
+
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "vijayvpillai",
+    database: "bookstore"
+});
+
+app.use(express.json());
+app.get("/", (req, res)=> {
+    res.json("server working");
+});
+
+app.get("/books", (req, res)=> {
+    const query = "SELECT * FROM store"
+    db.query(query,(err, data) => {
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+});
+
+app.post("/books", (req, res)=> {
+    const query = "INSERT INTO store (title, description, cover) VALUES (?)";
+    const values = [
+        req.body.title,
+        req.body.description,
+        req.body.cover
+    ];
+    db.query(query, [values], (err, data) => {
+        if(err) return res.json(err);
+        return res.json("succesfully added book")
+    })
+})
+
+
+app.listen(8800,()=> console.log("app is listenting"));
